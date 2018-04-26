96cd66164f0914cc3109f7f623a5d705c4d39e77
Phil Curzon <phil@novelfs.org>
2018-04-26 22:30:50 +0100

    initial commit

---
+++ src/Main.purs
@@ -0,0 +1,20 @@
+module Main where
+
+import Prelude
+
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Maybe (fromMaybe)
+import Routing ((</>), constantR, numberR)
+import Routing as R
+
+testHandler :: forall e. Number -> Number -> Eff (console :: CONSOLE | e) Unit
+testHandler x y = log $ "Did some shit " <> show x <> " " <> show y
+
+main :: forall e. Eff (console :: CONSOLE | e) Unit
+main = do
+  let combined = constantR "test" </> numberR </> numberR
+  let result = R.runRoute (testHandler) "test/21.7e6" combined
+  let result2 = R.runRoute (log "test") "test" (constantR "test")
+  --let test = map (\f -> f 6) result
+  fromMaybe (log "Nothing") $ result
---
+++ src/Routing.purs
@@ -0,0 +1,61 @@
+module Routing where
+
+import Prelude
+
+import Data.Int as I
+import Data.Maybe (Maybe(..))
+import Data.Number as N
+import Data.String (Pattern(..), stripPrefix)
+import Data.Tuple (Tuple(..), fst)
+
+
+class Route a b c | a -> b, b -> c where
+  run :: b -> String -> a -> Maybe (Tuple c String)
+
+data ConstantRoute
+  = ConstantRoute String
+
+data ParseRoute a
+  = ParseRoute (String -> Maybe a)
+
+data CombinedRoute r1 r2
+  = CombinedRoute (Tuple r1 r2)
+
+instance constantRouteRoute :: Route (ConstantRoute) a a where
+  run input str (ConstantRoute constStr) = map (\str' -> Tuple input str') $ stripPrefix (Pattern $ constStr <> "/") str
+
+instance parseRouteRoute :: Route (ParseRoute b) (b -> a) a where
+  run f str (ParseRoute parser) = map (\x -> Tuple (f x) str) $ parser str
+
+instance combinedRouteToute :: (Route a b c, Route d c e) => Route (CombinedRoute a d) b e where
+  run input str (CombinedRoute(Tuple r1 r2)) = do
+    Tuple a str' <- runRoute1
+    t2 <- run a str' r2
+    Just $ t2
+    where 
+      runRoute1 :: Maybe (Tuple c String)
+      runRoute1 = run input str r1
+
+constantR :: String -> ConstantRoute
+constantR name = ConstantRoute name
+
+intR :: ParseRoute Int
+intR = ParseRoute (I.fromString)
+
+numberR :: ParseRoute Number
+numberR = ParseRoute (N.fromString)
+
+stringR :: ParseRoute String
+stringR = ParseRoute (Just)
+
+combineR :: forall a b c d e . Route a b c => Route d c e => a -> d -> CombinedRoute a d
+combineR a b = CombinedRoute (Tuple a b)
+
+infix 4 combineR as </>
+
+runRoute :: forall a b c. Route a b c => b -> String -> a -> Maybe c
+runRoute x str v = 
+  map fst $ run x str v
+
+
+
