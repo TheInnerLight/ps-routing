45cf8e79eaa6204b73259cbd0c2873482c6906db
Phil Curzon <phil@novelfs.org>
2018-05-22 00:33:45 +0100

    Make routing work for multiple routes

--- src/Main.purs
+++ src/Main.purs
@@ -6,7 +6,9 @@ import Control.Monad.Aff (launchAff_)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.List (List(..), fromFoldable)
 import Express (listenHttp)
+import Express.Application (ApplicationInternal(..), run)
 import Express.Effect (EXPRESS)
 import Express.Endpoint (Endpoint, createHandler, mkEndpoint)
 import Express.Http (NOT_FOUND, OK)
@@ -15,14 +17,25 @@ import Paths (constantPath, intPath, numberPath, (</>))
 
 endpointTest :: forall e s. Endpoint (console :: CONSOLE | e) (ok :: OK, notFound :: NOT_FOUND | s)
 endpointTest = mkEndpoint 
-  { path : constantPath "cheese" </> intPath </> numberPath </> constantPath "blob" 
-  , handler : \x y -> do
-    _ <- liftEff $ log "Received endpoint request"
-    pure $ ok $ text "Hi test!" 
+  { path : constantPath "your-number" </> intPath
+  , handler : \x-> do
+    _ <- liftEff $ log "Received number endpoint request"
+    pure $ ok $ text $ "Your number is " <> show x
   }
+
+endpointTest2 :: forall e s. Endpoint (console :: CONSOLE | e) (ok :: OK, notFound :: NOT_FOUND | s)
+endpointTest2 = mkEndpoint 
+  { path : constantPath "cheese"
+  , handler : do
+    _ <- liftEff $ log "Received cheese endpoint request"
+    pure $ ok $ text "mimolette"
+  }
+
+--application :: forall e. ApplicationInternal (console :: CONSOLE | e)
+application = ApplicationInternal (fromFoldable [endpointTest, endpointTest2])
 
 main :: ∀ e. Eff (console :: CONSOLE, express :: EXPRESS | e) Unit
 main = launchAff_ do
-  _ <- listenHttp (createHandler endpointTest) 8056
+  _ <- run application
   pure unit
 
---
+++ src/application.purs
@@ -0,0 +1,41 @@
+module Express.Application where
+  
+import Prelude
+
+import Control.Monad.Aff.Class (class MonadAff)
+import Control.Monad.Eff.Console (log)
+import Control.Monad.Error.Class (class MonadError, catchError, throwError)
+import Data.List (List(..), foldr, length)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Express (listenHttp)
+import Express.Effect (EXPRESS)
+import Express.Endpoint (Endpoint, createHandler)
+import Express.Http (HttpError(..), NOT_FOUND, kind Status)
+import Express.Response (notFound, text)
+import Unsafe.Coerce (unsafeCoerce)
+
+newtype ApplicationInternal s eff = ApplicationInternal (List (Endpoint eff s))
+
+findM :: forall m t. Monad m => (t -> m Boolean) -> List t -> m (Maybe t)
+findM p xs =
+  skipWhileMRec p xs
+  where 
+    skipWhileMRec p Nil = pure Nothing
+    skipWhileMRec p (Cons x xs') = do
+      q <- p x
+      case q of
+        true  -> pure $ Just x
+        false -> skipWhileMRec p xs'
+    
+
+run :: forall eff s m. MonadAff (express :: EXPRESS | eff) m => ApplicationInternal (notFound :: NOT_FOUND | s) eff -> m Unit
+run (ApplicationInternal items) =
+  listenHttp handleShit 8056
+  where
+    handleShit = do 
+      ep <- findM f items
+      fromMaybe (pure $ notFound $ text "Not Found!") (map (createHandler) ep)
+    f endpoint = catchError (map (\_ -> true) $ createHandler endpoint) (pure <<< errorhandler)
+    errorhandler UnknownPath = false
+
+        
